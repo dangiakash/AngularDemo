f26627db96b7e0b64ff201021bf9b427c0838528
akashdangi019 <akash.dangi@jadeglobal.com>
2019-05-29 17:00:46 +0530

    Second Commit

--- src/app/department/department.component.ts
+++ src/app/department/department.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Route1DepartmentComponent implements OnInit {
 
+  title = 'DemoTitle';
+  
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { 
   }
 
 }
