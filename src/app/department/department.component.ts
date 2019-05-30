5f68c90883621d5a81a85b149668e9c66793fdfd
dangiakash <36843515+dangiakash@users.noreply.github.com>
2019-05-30 19:14:40 +0530

    Update department.component.ts

--- src/app/department/department.component.ts
+++ src/app/department/department.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class Route1DepartmentComponent implements OnInit {
 
  
-  DemoTitle = "DemoTitle1";
+ DemoTitless = ''
     
   constructor() { }
 
