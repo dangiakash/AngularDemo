727561da2ec83db39df593906bb6124a84a49b03
dangiakash <36843515+dangiakash@users.noreply.github.com>
2019-05-31 12:59:33 +0530

    Update department.component.ts

--- src/app/department/department.component.ts
+++ src/app/department/department.component.ts
@@ -7,16 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Route1DepartmentComponent implements OnInit {
 
- DemoVar = ''
+ DemoVar = '';
+  
+  DemoVar2 = '';
+  DemoVar12 = '';
+  DemoVar11 = '';
+  
+  
+  
 
     
-  constructor() { }
+  constructor() { 
+  
+  DemoVar7 = '';
+    DemoVar6 = '';
+    DemoVar5 = '';
+    DemoVar4 = '';
+  }
 
   ngOnInit() { 
     console.log('First Error')
     console.error('Second Error')     //  red
     console.info('Third Error')
     console.warn('Fourth Error')      //  yellow
+    
+    DemoVar10 = '';
+    DemoVar9 = '';
+    DemoVar8 = '';
   }
 
 }
